1b0ba210299615de56f21c81955bceac13d8dc14
Vladimir_Smirnov <mcgohnpro@gmail.com>
2024-04-24 23:23:10 +0300

    refactoring with using hooks task-editor component.

--- src/components/task/task-editor/task-editor.jsx
+++ src/components/task/task-editor/task-editor.jsx
@@ -1,59 +1,44 @@
-import { Component } from 'react'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
-export default class TaskEditor extends Component {
-  constructor(props) {
-    super(props)
-    const { title } = props
-    this.state = {
-      value: title,
-    }
-    this.handleEditTitle = this.handleEditTitle.bind(this)
+export default function TaskEditor(props) {
+  const { editing, confirmTitleChanges, title, abortTitleChanges } = props
+  const [value, setValue] = useState(title)
+
+  const handleEditTitle = (e) => {
+    setValue(e.target.value)
   }
 
-  handleEditTitle(e) {
-    this.setState({
-      value: e.target.value,
-    })
-  }
+  if (!editing) return null
 
-  render() {
-    const { value } = this.state
-    const { editing, confirmTitleChanges, title, abortTitleChanges } = this.props
-    if (!editing) return null
-    return (
-      <form
-        action=""
-        onSubmit={(e) => {
-          e.preventDefault()
-          confirmTitleChanges(value)
+  return (
+    <form
+      action=""
+      onSubmit={(e) => {
+        e.preventDefault()
+        confirmTitleChanges(value)
+      }}
+    >
+      <input
+        // eslint-disable-next-line jsx-a11y/no-autofocus
+        autoFocus
+        className="edit"
+        type="text"
+        onChange={handleEditTitle}
+        value={value}
+        onBlur={() => {
+          setValue(title)
+          abortTitleChanges()
         }}
-      >
-        <input
-          // eslint-disable-next-line jsx-a11y/no-autofocus
-          autoFocus
-          className="edit"
-          type="text"
-          onChange={this.handleEditTitle}
-          value={value}
-          onBlur={() => {
-            this.setState({
-              value: title,
-            })
+        onKeyDown={(e) => {
+          if (e.code === 'Escape') {
+            setValue(title)
             abortTitleChanges()
-          }}
-          onKeyDown={(e) => {
-            if (e.code === 'Escape') {
-              this.setState({
-                value: title,
-              })
-              abortTitleChanges()
-            }
-          }}
-        />
-      </form>
-    )
-  }
+          }
+        }}
+      />
+    </form>
+  )
 }
 
 TaskEditor.defaultProps = {
