d365d922a75b200824c823645a05c83a511c02bf
Vladimir_Smirnov <mcgohnpro@gmail.com>
2024-04-25 23:01:59 +0300

    Reafctoring new-task-form component into function with hooks

--- src/components/new-task-form/new-task-form.jsx
+++ src/components/new-task-form/new-task-form.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import './new-task-form.css'
 import PropTypes from 'prop-types'
 
@@ -6,79 +6,73 @@ import getId from '../../utils'
 
 const isNumber = (value) => (Number.isFinite(+value) ? value : '')
 
-export default class NewTaskForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: '',
-      min: '',
-      sec: '',
-    }
-  }
+export default function NewTaskForm(props) {
+  const [value, setValue] = useState('')
+  const [min, setMin] = useState('')
+  const [sec, setSec] = useState('')
 
-  render() {
-    const { createTask } = this.props
-    const { value, min, sec } = this.state
-    return (
-      <form
-        className="new-todo-form"
-        action=""
-        onSubmit={(e) => {
-          e.preventDefault()
-          createTask({
-            id: getId(),
-            title: value,
-            completed: false,
-            created: new Date(),
-            totalSeconds: parseInt(min, 10) * 60 + parseInt(sec, 10),
-          })
-          this.setState({ value: '', min: '', sec: '' })
+  const { createTask } = props
+  return (
+    <form
+      className="new-todo-form"
+      action=""
+      onSubmit={(e) => {
+        e.preventDefault()
+        createTask({
+          id: getId(),
+          title: value,
+          completed: false,
+          created: new Date(),
+          totalSeconds: parseInt(min, 10) * 60 + parseInt(sec, 10),
+        })
+        setValue('')
+        setMin('')
+        setSec('')
+      }}
+    >
+      <input
+        name="task"
+        required
+        autoComplete="off"
+        className="new-todo"
+        placeholder="Task"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value.trimStart())
         }}
-      >
-        <input
-          name="task"
-          required
-          autoComplete="off"
-          className="new-todo"
-          placeholder="Task"
-          value={value}
-          onChange={(e) => {
-            this.setState({ value: e.target.value.trimStart() })
-          }}
-        />
-        <input
-          required
-          pattern="^(0?[0-9]|[1-5][0-9]|59)$"
-          title="Enter value from 0 to 59"
-          autoComplete="off"
-          maxLength={2}
-          name="min"
-          className="new-todo-form__timer"
-          placeholder="Min"
-          value={min}
-          onChange={(e) => {
-            this.setState({ min: isNumber(e.target.value.trimStart().trimEnd()) })
-          }}
-        />
-        <input
-          required
-          pattern="^(0?[0-9]|[1-5][0-9]|59)$"
-          title="Enter value from 0 to 59"
-          autoComplete="off"
-          name="sec"
-          type="text"
-          maxLength={2}
-          className="new-todo-form__timer"
-          placeholder="Sec"
-          value={sec}
-          onChange={(e) => {
-            this.setState({ sec: isNumber(e.target.value.trimStart().trimEnd()) })
-          }}
-        />
-        <input className="new-todo-form__submit" type="submit" />
-      </form>
-    )
-  }
+      />
+      <input
+        required
+        pattern="^(0?[0-9]|[1-5][0-9]|59)$"
+        title="Enter value from 0 to 59"
+        autoComplete="off"
+        maxLength={2}
+        name="min"
+        className="new-todo-form__timer"
+        placeholder="Min"
+        value={min}
+        onChange={(e) => {
+          setMin(isNumber(e.target.value.trimStart().trimEnd()))
+        }}
+      />
+      <input
+        required
+        pattern="^(0?[0-9]|[1-5][0-9]|59)$"
+        title="Enter value from 0 to 59"
+        autoComplete="off"
+        name="sec"
+        type="text"
+        maxLength={2}
+        className="new-todo-form__timer"
+        placeholder="Sec"
+        value={sec}
+        onChange={(e) => {
+          setSec(isNumber(e.target.value.trimStart().trimEnd()))
+        }}
+      />
+      <input className="new-todo-form__submit" type="submit" />
+    </form>
+  )
 }
 
 NewTaskForm.defaultProps = {
